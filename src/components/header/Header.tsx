eca8133920596d12107d9539508c761acc928ba1
m.kane <michael.kane840@gmail.com>
2023-10-11 19:30:36 +0100

    Drawer added with single demo button

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,22 +2,13 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Login from './Login';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import Drawer from '../uploadsDrawer/Drawer';
 
 const Header = () => {
     return (
         <AppBar className='pageheader' position="static">
             <Toolbar>
-                <IconButton
-                    size="large"
-                    edge="start"
-                    color="inherit"
-                    aria-label="menu"
-                    sx={{ mr: 2 }}
-                >
-                    <MenuIcon />
-                </IconButton>
+                <Drawer />
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                     PCM Sim Game
                 </Typography>
