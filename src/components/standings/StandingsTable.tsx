fc272294a630ec2b5475afe41baccb90f20d11f5
m.kane <michael.kane840@gmail.com>
2023-10-12 20:10:04 +0100

    Sorting functional

---
+++ src/components/standings/StandingsTable.tsx
@@ -0,0 +1,178 @@
+import React from "react"
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import TableSortLabel from '@mui/material/TableSortLabel';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import RaceDaysModal from '../RaceDaysModal';
+import { styled } from '@mui/material/styles';
+import { visuallyHidden } from '@mui/utils';
+
+interface Data {
+    team: string;
+    points: number;
+}
+
+interface HeadCell {
+    disablePadding: boolean;
+    id: keyof Data;
+    label: string;
+    numeric: boolean;
+}
+
+type Order = 'asc' | 'desc';
+
+interface EnhancedTableProps {
+    onRequestSort: (event: React.MouseEvent<unknown>, property: keyof Data) => void;
+    order: Order;
+    orderBy: string;
+}
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+        border: 0,
+    },
+}));
+
+const headCells: readonly HeadCell[] = [
+    {
+        id: 'team',
+        numeric: false,
+        disablePadding: true,
+        label: 'TeamName',
+    },
+    {
+        id: 'points',
+        numeric: false,
+        disablePadding: true,
+        label: 'UCI Points',
+    }
+]
+
+const uciStandingsData = [{ team: 'bingoal', points: 30000 },
+{ team: 'jumbo', points: -100 },
+{ team: 'ef education', points: 30000 },
+{ team: 'ineos', points: -30 }
+];
+
+const EnhancedTableHead = (props: EnhancedTableProps) => {
+    const { order, orderBy, onRequestSort } = props;
+    const createSortHandler =
+        (property: keyof Data) => (event: React.MouseEvent<unknown>) => {
+            onRequestSort(event, property);
+        };
+
+    return (
+        <TableHead>
+            <TableRow>
+                {headCells.map((headCell) => (
+                    <TableCell
+                        key={headCell.id}
+                        align={headCell.numeric ? 'right' : 'left'}
+                        padding={headCell.disablePadding ? 'none' : 'normal'}
+                        sortDirection={orderBy === headCell.id ? order : false}
+                    >
+                        <TableSortLabel
+                            active={orderBy === headCell.id}
+                            direction={orderBy === headCell.id ? order : 'asc'}
+                            onClick={createSortHandler(headCell.id)}
+                        >
+                            {headCell.label}
+                            {orderBy === headCell.id ? (
+                                <Box component="span" sx={visuallyHidden}>
+                                    {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                                </Box>
+                            ) : null}
+                        </TableSortLabel>
+                    </TableCell>
+                ))}
+            </TableRow>
+        </TableHead>
+    );
+}
+
+function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+    if (b[orderBy] < a[orderBy]) {
+        return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+        return 1;
+    }
+    return 0;
+}
+
+function getComparator<Key extends keyof any>(
+    order: Order,
+    orderBy: Key,
+): (
+    a: { [key in Key]: number | string },
+    b: { [key in Key]: number | string },
+) => number {
+    return order === 'desc'
+        ? (a, b) => descendingComparator(a, b, orderBy)
+        : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function stableSort<T>(array: readonly T[], comparator: (a: T, b: T) => number) {
+    const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
+    stabilizedThis.sort((a, b) => {
+        const order = comparator(a[0], b[0]);
+        if (order !== 0) {
+            return order;
+        }
+        return a[1] - b[1];
+    });
+    return stabilizedThis.map((el) => el[0]);
+}
+
+const StandingsTable = () => {
+    const [order, setOrder] = React.useState<Order>('asc');
+    const [orderBy, setOrderBy] = React.useState<keyof Data>('points');
+
+    const handleRequestSort = (
+        event: React.MouseEvent<unknown>,
+        property: keyof Data,
+    ) => {
+        const isAsc = orderBy === property && order === 'asc';
+        setOrder(isAsc ? 'desc' : 'asc');
+        setOrderBy(property);
+    };
+
+    const sortedStandings = React.useMemo(
+        () =>
+            stableSort(uciStandingsData, getComparator(order, orderBy)),
+        [order, orderBy],
+    );
+
+    return (
+        <TableContainer className="standingsTableBack">
+            <Table component={Paper} className='standingsTable' aria-label="customized table">
+                <EnhancedTableHead
+                    order={order}
+                    orderBy={orderBy}
+                    onRequestSort={handleRequestSort}
+                />
+                <TableBody>
+                    {sortedStandings.map((row, index) => (
+                        <StyledTableRow key={row.team} >
+                            <TableCell>
+                                <RaceDaysModal>{row.team as string}</RaceDaysModal>
+                            </TableCell>
+                            <TableCell>{row.points}</TableCell>
+                        </StyledTableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    )
+};
+
+export default StandingsTable;
