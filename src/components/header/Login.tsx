88973b0c085595ef2529b4797223eca7a9a1f744
m.kane <michael.kane840@gmail.com>
2023-10-15 16:41:06 +0100

    login styling fix

--- src/components/header/Login.tsx
+++ src/components/header/Login.tsx
@@ -1,6 +1,18 @@
-import { Modal, Button, FormGroup, FormControlLabel, FormControl } from "@mui/material";
-import TextField from '@mui/material/TextField';
+import { Modal, Button, FormGroup, FormControlLabel, FormControl, TextField, Divider } from "@mui/material";
+import Box from '@mui/material/Box';
 import React from "react"
+
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
 
 const Login = () => {
     const [open, setOpen] = React.useState(false);
@@ -11,26 +23,18 @@ const Login = () => {
         <div>
             <Button onClick={handleOpen} color="inherit">Login</Button>
             <Modal
-                className="LoginModal"
                 open={open}
                 onClose={handleClose}
                 aria-labelledby="modal-modal-title"
                 aria-describedby="modal-modal-description">
-                <form className='LoginForm'>
-                    <label>
-                        <p>Username</p>
-                        <input type="text" />
-                    </label>
-                    <label>
-                        <p>Password</p>
-                        <input type="password" />
-                    </label>
-                    <div>
-                        <button type="submit">Submit</button>
-                    </div>
-                </form>
+                <Box sx={style}>
+                    <TextField id="outlined-basic" label="Username" variant="outlined" />
+                    <TextField id="outlined-basic" label="Password" variant="outlined" />
+                    <Divider />
+                    <Button>Login</Button>
+                </Box>
             </Modal>
-        </div>
+        </div >
     );
 };
 
