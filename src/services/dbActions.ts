7cb9cdb92a14453c7a59f9a0d429c281a559a9a2
m.kane <michael.kane840@gmail.com>
2024-02-10 07:48:48 +0000

    basic functionality with google sheets

--- src/services/dbActions.ts
+++ src/services/dbActions.ts
@@ -1,57 +1,11 @@
-import { getDocs, collection, setDoc, doc, query, where } from "firebase/firestore";
 import { getDb } from "./db"
 
-const simGameCollection = "sim-game"
-const loginCollection = "login"
-
 export const getUCIStandings = async () => {
-    const doc_refs = await getDocs(collection(getDb(), simGameCollection))
-
-    const res: any = [];
-
-    doc_refs.forEach(standing => {
-        res.push({
-            id: standing.id,
-            ...standing.data()
-        })
-    })
-
-    return res;
+    const doc=await getDb();
+    return doc.sheetsByIndex[0].getRows();
 }
 
 export const getTeamUciStanding = async (teamName: string) => {
-    const teamCollectionRef = collection(getDb(), simGameCollection)
-    const teamStandingQuery = query(teamCollectionRef, where("team", "==", teamName))
-    const doc_refs = await getDocs(teamStandingQuery);
-
-    const res:any = [];
-
-    doc_refs.forEach(country => {
-        res.push({
-            id: country.id,
-            ...country.data()
-        })
-    })
-
-    return res;
+    const doc=await getDb();
+    return doc.sheetsByIndex[0];
 };
-
-export const getLogin = async () => {
-    const doc_refs = await getDocs(collection(getDb(), loginCollection))
-
-    const res: any = [];
-
-    doc_refs.forEach(loginDetails => {
-        res.push({
-            id: loginDetails.id,
-            ...loginDetails.data()
-        })
-    })
-
-    return res;
-}
-
-export const updateStandings = (args: any) => {
-    const { id, ...params } = args
-    return setDoc(doc(getDb(), simGameCollection, id), params)
-}
