c0b4b0f0f805da4be77b0dfe98ea5a35871d3161
m.kane <michael.kane840@gmail.com>
2024-02-15 20:13:31 +0000

    results done

--- src/components/raceQuerys/resultsTabs/resultsUtils.ts
+++ src/components/raceQuerys/resultsTabs/resultsUtils.ts
@@ -36,6 +36,8 @@ const getFinal = (results:Array<GoogleSpreadsheetRow>) => results.map((row)=> {
 });
 
 export const getFinalGc = (results:Array<GoogleSpreadsheetRow>) => getFinal(results).slice(1,51);
+export const getFinalPoints = (results:Array<GoogleSpreadsheetRow>) => getFinal(results).slice(52,61);
+export const getFinalKOM = (results:Array<GoogleSpreadsheetRow>) => getFinal(results).slice(62,73);
 export const getFinalYouth = (results:Array<GoogleSpreadsheetRow>) => getFinal(results).slice(74,84);
 
 //utility functions to grab provisional gc/youth/kom/points
@@ -44,5 +46,29 @@ export const getProvisionalGc = (results:Array<GoogleSpreadsheetRow>) => results
         rider: row.get('proGcRider'),
         team: row.get('proGcTeam'),
         points: row.get('proGcPoints')
+    }
+});
+
+export const getProvisionalYouth = (results:Array<GoogleSpreadsheetRow>) => results.map((row)=> {
+    return {
+        rider: row.get('proYouthRider'),
+        team: row.get('proYouthTeam'),
+        points: row.get('proYouthPoints')
+    }
+});
+
+export const getProvisionalPoints = (results:Array<GoogleSpreadsheetRow>) => results.map((row)=> {
+    return {
+        rider: row.get('proPointsRider'),
+        team: row.get('proPointsTeam'),
+        points: row.get('proPointsPoints')
+    }
+});
+
+export const getProvisionalKom = (results:Array<GoogleSpreadsheetRow>) => results.map((row)=> {
+    return {
+        rider: row.get('proKomRider'),
+        team: row.get('proKomTeam'),
+        points: row.get('proKomPoints')
     }
 });
